c61d2631ca91af4269fb65d6ac405fae964782a3
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-11-07 16:47:55 +0530

    Initial

---
+++ src/app/api/actions/rps/route.ts
@@ -0,0 +1,23 @@
+import {
+  ActionPostResponse,
+  createActionHeaders,
+  createPostResponse,
+  ActionGetResponse,
+  ActionPostRequest,
+} from "@solana/actions";
+const headers = createActionHeaders({
+  chainId: "devnet", // or chainId: "devnet"
+  actionVersion: "2.2.1", // the desired spec version
+});
+export const GET = async (req: Request) => {
+  const payload: ActionGetResponse = {
+    title: "Actions Example - Simple On-chain Memo by Arpit",
+    icon: 'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/',
+    description: "Send a message on-chain using a Memo",
+    label: "Send Memo",
+  };
+
+  return Response.json(payload, {
+    headers,
+  });
+}

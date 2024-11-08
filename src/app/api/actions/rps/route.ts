f20f236c99d202452982e6f462f171b2b6f93f04
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-11-08 09:43:06 +0530

    Simple memo

--- src/app/api/actions/rps/route.ts
+++ src/app/api/actions/rps/route.ts
@@ -4,20 +4,123 @@ import {
   createPostResponse,
   ActionGetResponse,
   ActionPostRequest,
+  MEMO_PROGRAM_ID,
 } from "@solana/actions";
+
+import { 
+  clusterApiUrl,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionInstruction
+} from "@solana/web3.js";
+
 const headers = createActionHeaders({
   chainId: "devnet", // or chainId: "devnet"
   actionVersion: "2.2.1", // the desired spec version
 });
 export const GET = async (req: Request) => {
   const payload: ActionGetResponse = {
-    title: "Actions Example - Simple On-chain Memo by Arpit",
-    icon: 'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/',
-    description: "Send a message on-chain using a Memo",
+    title: "Testing by Arpit",
+    icon: new URL(
+      "/icon.png",
+      new URL(req.url).origin
+    ).toString(),
+    description:
+      "Simple On-chain Memo Blinks: Send a message on-chain using Solana Memo program via Blinks.",
     label: "Send Memo",
+    links: 
+      {
+        actions: [
+          {
+            label: "Send Memo1",
+            href: req.url,
+            type: "transaction"
+          },
+
+        {
+          label: "Send Memo2",
+          href: req.url,
+          type: "transaction"
+        }
+      ]
+      }
   };
 
   return Response.json(payload, {
     headers,
   });
-}
+};
+
+export const OPTIONS = GET;
+
+
+export const POST = async (req: Request) => {
+  try {
+    const body: ActionPostRequest = await req.json();
+    // Validate to confirm the user publickey received is valid before use
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      return new Response('Invalid "account" provided', {
+        status: 400,
+        headers, //Must include CORS HEADERS
+      });
+    }
+
+    // Solana Blockchain Cluster (Set Mainnet "mainnet-beta" or Devnet "devnet")
+    // If your RPC not present, it will use default devnet RPC provided to us via web3.js "clusterApiUrl("devnet")"
+    // NOTE: "clusterApiUrl("devnet")" is not for mainnet use - for mainnet production launched Blinks, get your own RPC
+    // For testing on mainnet - use "mainnet-beta"
+    const connection = new Connection(
+      process.env.SOLANA_RPC! || clusterApiUrl("devnet")
+    );
+
+    const transaction = new Transaction().add(
+      // note: `createPostResponse` requires at least 1 non-memo instruction
+      ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: 1000,
+      }),
+      new TransactionInstruction({
+        programId: new PublicKey(MEMO_PROGRAM_ID),
+        data: Buffer.from(
+          "This is a simple memo message from Arpit.",
+          "utf8"
+        ),
+        keys: [],
+      })
+    );
+
+    // set the end user as the fee payer
+    transaction.feePayer = account;
+
+    // Get the latest Block Hash
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        type: "transaction",
+        transaction,
+        message: "Post this memo on-chain",
+      },
+      // no additional signers are required for this transaction
+      // signers: [],
+    });
+
+    return Response.json(payload, {
+      headers,
+    });
+  } catch (err) {
+    console.log(err);
+    let message = "An unknown error occurred";
+    if (typeof err == "string") message = err;
+    return new Response(message, {
+      status: 400,
+      headers, //Must include CORS HEADERS
+    });
+  }
+};

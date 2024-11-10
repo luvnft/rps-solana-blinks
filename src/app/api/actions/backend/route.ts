061a36a482c8ecfab981616a0d3c5d1541a035d0
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-11-10 13:01:44 +0530

    claim money in result

--- src/app/api/actions/backend/route.ts
+++ src/app/api/actions/backend/route.ts
@@ -111,6 +111,39 @@ export const POST = async (req: Request) => {
                     title: "You Won.",icon: new URL("/icon.png",new URL(req.url).origin).toString(),
                     description: "Let's play Rock Paper Scissors! If you win you get DOUBLE your betted SOL, if it's a tie you get your betted SOL back, and if you lose you lose your betted SOL.",
                     label: "Rock Paper Scissors",
+                    "links": {
+                    "actions": [
+                        {
+                        "label": "Play!", // button text
+                        "href": "/api/actions/backend?result={amount}&choice={choice}",
+                        "parameters": [
+                            {
+                            type: "select",
+                            name: "amount", // parameter name in the `href` above
+                            label: "Bet Amount in SOL", // placeholder of the text input
+                            required: true,
+                            options: [
+                                { label: "1 SOL", value: "1" },
+                                { label: "0.1 SOL", value: "0.1" },
+                                { label: "0.01 SOL", value: "0.01" },
+                            ],
+                            },
+                            {
+                            type: "radio",
+                            name: "choice", // parameter name in the `href` above
+                            label: "Choose your move?", // placeholder of the text input
+                            required: true,
+                            options: [
+                                { label: "Rock", value: "R" },
+                                { label: "Paper", value: "P" },
+                                { label: "Scissors", value: "S" },
+                            ],
+                            },
+                        ],
+                        type: "transaction"
+                        }
+                    ]
+                    }
                 },
             },
           },

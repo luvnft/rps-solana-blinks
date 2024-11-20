5abfecec3110a7efa4600976806633f0654ee525
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-11-20 15:05:19 +0530

    chainid

--- src/app/api/actions/backend/route.ts
+++ src/app/api/actions/backend/route.ts
@@ -61,7 +61,7 @@ export const POST = async (req: Request) => {
     // NOTE: "clusterApiUrl("devnet")" is not for mainnet use - for mainnet production launched Blinks, get your own RPC
     // For testing on mainnet - use "mainnet-beta"
     const connection = new Connection(
-      process.env.Net! || clusterApiUrl("mainnet-beta")
+      clusterApiUrl("devnet")
     );
     const web3 = require("@solana/web3.js");
     const sender = Keypair.fromSecretKey(bs58.decode(process.env.SOLANA_SENDER_SECRET!));
